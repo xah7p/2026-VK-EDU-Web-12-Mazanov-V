068f0bb6eed2f82bf4884dc2a9d51d8965a7f792
xah7p <uglupia@gmail.com>
2026-04-07 19:59:07 +0300

    added email validation

--- questions/views.py
+++ questions/views.py
@@ -69,7 +69,7 @@ class HomePageView(TemplateView):
         return context
 
 class HotPageView(TemplateView):
-    template_name = 'questions/index.html'
+    template_name = 'questions/hot.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
